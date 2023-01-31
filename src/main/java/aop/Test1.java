6f9bbe9cc2b64758005d25d134f81acc8b965c95
VMurtazin <obdorsk@gmail.com>
2023-01-31 22:58:21 +0500

    PointCutCombination

--- src/main/java/aop/Test1.java
+++ src/main/java/aop/Test1.java
@@ -6,9 +6,15 @@ public class Test1 {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context=
                 new AnnotationConfigApplicationContext(MyConfig.class);
-        Library library=context.getBean("libraryBean",Library.class);
+        UniLibrary uniLibrary=context.getBean("uniLibrary", UniLibrary.class);
+//        SchoolLibrary schoolLibrary=context.getBean("schoolLibrary", SchoolLibrary.class);
 
-        library.getBook();
+        uniLibrary.getBook("Kruzo");
+//        uniLibrary.returnBook();
+//        uniLibrary.getMagazine();
+
+
+//        schoolLibrary.getBook();
         context.close();
     }
 }
